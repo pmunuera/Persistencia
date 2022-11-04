b088c4882c2890f8dcb2ab4f81793afe0952b23f
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-11-04 19:24:12 +0100

    Commit write file

--- app/src/main/java/com/example/persistencia/MainActivity.java
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.persistencia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+
+        try{
+            OutputStream os = openFileOutput("dades.txt",MODE_PRIVATE);
+            String message = "hola q tal";
+            os.write(message.getBytes());
+            os.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
