9903e91937bdeffbefca4362081adae968ff1862
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-11-07 19:19:13 +0100

    Commit write file

--- app/src/main/java/com/example/persistencia/MainActivity.java
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.persistencia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.OutputStream;
 
@@ -13,17 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button b = findViewById(R.id.button);
-
         try{
             OutputStream os = openFileOutput("dades.txt",MODE_PRIVATE);
-            String message = "hola q tal";
+            String message = "hola q tal\n ";
             os.write(message.getBytes());
             os.close();
         }
         catch(Exception e){
             e.printStackTrace();
         }
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try{
+                    OutputStream os = openFileOutput("dades.txt",MODE_PRIVATE);
+                    String message = "hola q tal";
+                    TextView text = findViewById(R.id.textInputEditText);
+                    String secondMessage=String.valueOf(text.getText());
+                    os.write(message.getBytes());
+                    os.write(secondMessage.getBytes());
+                    os.close();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        );
     }
 }
